9ceaaad5ed073470e6d3cb06bff6d30b77df7f6a
jason-hodson <jason.hodson11@gmail.com>
2025-09-27 13:48:07 -0500

    Delete examples_code/chapter_4.py

--- examples_code/chapter_4.py
+++
@@ -1,8 +0,0 @@
-#import pandas package
-import pandas as pd
-
-#read in the specific file you’re trying to read in
-df = pd.read_csv(“Retail Sales File 1.xlsx”)
-
-#read in excel file with read_excel function
-df = pd.read_excel(“Retail Sales File 1.xlsx”)
